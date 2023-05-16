33549565e02b341a82478987e8d4bf604c8cac8d
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-05-16 20:44:16 -0300

    deleted Button component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from '@/components/Button'
-
 export default function Home() {
   return (
-    <h1>
-      HELLOU
-      <Button title="Falk" />
-      <Button title="Noazin" />
-      <Button title="Lucas" />
-
-    </h1>
+    <div>
+      oi
+    </div>
   )
 }

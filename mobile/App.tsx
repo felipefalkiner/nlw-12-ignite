97bd66bd6112969a491753f4eaa8075627b2cbb3
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-05-17 23:03:25 -0300

    configured project fonts for mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import {BaiJamjuree_700Bold} from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if(!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className='bg-gray-950 h-20 flex-1 items-center justify-center'>
-      <Text className='text-gray-50 font-bold text-5xl text-center'>We have Tailwind!</Text>
+    <View className='bg-gray-900 h-20 flex-1 items-center justify-center'>
+      <Text className='text-gray-50 font-title text-5xl text-center'>We have Tailwind!</Text>
       <StatusBar style="light" translucent/>
     </View>
   );

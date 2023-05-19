e88dd839bb1b11b54a0257cf9b9a80a6c43a9b9e
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-05-19 19:23:30 -0300

    implemented Github Auth to return User Code on Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,8 +8,16 @@ import blurBg from './src/assets/bg-blur.png';
 import Stripes from './src/assets/stripes.svg';
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg';
 import { styled } from 'nativewind';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
 
 const StyledStripes = styled(Stripes);
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/a24e11a4604d2d3a0e21',
+};
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -17,6 +25,33 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'a24e11a4604d2d3a0e21',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
+  
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    // console.log(response);
+
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
+
 
   if (!hasLoadedFonts) {
     return null
@@ -35,7 +70,7 @@ export default function App() {
           <Text className='text-center font-title text-2xl leading-tight text-gray-50'>Sua cápsula do tempo</Text>
           <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
         </View>
-        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-2'>
+        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-2' onPress={() => signInWithGithub()}>
           <Text className='font-alt text-sm uppercase text-black'>
             Cadastrar lembrança
           </Text>
